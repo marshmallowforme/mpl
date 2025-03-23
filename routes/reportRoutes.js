006e9f3129f1ccab1acb73ee54f55a2f9aa96a31
marshmallowforme <104122985+marshmallowforme@users.noreply.github.com>
2025-03-23 17:27:08 +0530

    Delete routes directory

--- routes/reportRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reportController = require('../controllers/reportController');
-
-router.get('/sales', reportController.getSalesReport);
-router.get('/users', reportController.getUserReport);
-router.post('/generate', reportController.generateReport);
-
-module.exports = router; // Correct export
